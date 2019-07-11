25e6912abe0f7cd6acc93c68d27fbb77b41bf043
Norbert Cyran <cyran.norbert97@gmail.com>
2019-07-11 21:42:39 +0200

    Remove dotenv from settings (heroku check)

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import dotenv
+#import dotenv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
+#dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
