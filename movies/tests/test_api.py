ab4e43d9a26b94aead239786ab7034a0f3a3b00c
Norbert Cyran <cyran.norbert97@gmail.com>
2019-07-11 22:59:43 +0200

    Add test case for APIException in movies endpoint

--- movies/tests/test_api.py
+++ movies/tests/test_api.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from rest_framework.test import APITestCase
 
 from .fixtures import PREPROCESSED_RESPONSE
+from ..exceptions import OMDBApiException
 from ..models import Comment
 
 
@@ -36,7 +37,7 @@ class MoviesAPITestCase(APITestCase):
 
         self.assertListEqual(expected, response.data)
 
-    @mock.patch('movies.utils.fetch_movie_data')
+    @mock.patch('movies.serializers.fetch_movie_data')
     def test_create_movie(self, fetch_mock):
         """Movie is fetched from OMDb and inserted into app db."""
         fetch_mock.return_value = PREPROCESSED_RESPONSE
@@ -46,6 +47,17 @@ class MoviesAPITestCase(APITestCase):
 
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.data['title'], 'A Clockwork Orange')
+
+    @mock.patch('movies.serializers.fetch_movie_data')
+    def test_create_movie__api_exception(self, fetch_mock):
+        """Response with 400 status code and error message is returned."""
+        fetch_mock.side_effect = OMDBApiException('Movie not found.')
+
+        path = reverse('movie-list')
+        response = self.client.post(path, data={'title': 'Non existent'})
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['detail'], 'Movie not found.')
 
     @staticmethod
     def _create_comment(movie_id, created):
