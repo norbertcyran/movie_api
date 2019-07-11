531bfad4a4723842b9671e52bb285e80589261a4
Norbert Cyran <cyran.norbert97@gmail.com>
2019-07-11 20:57:28 +0200

    Add whitenoise middleware for deploying on heroku

--- movie_api/settings.py
+++ movie_api/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 REST_FRAMEWORK = {
@@ -133,6 +134,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 OMDB_API_KEY = os.environ['OMDB_API_KEY']
 
